24d2f48b98b620649fbe1ebb8a674bdc7cdf7a06
Jung Lee <lee.jung.s95@gmail.com>
2020-05-20 02:48:44 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -16,7 +16,7 @@ class Anagram
 
   def match(anagrams_array)
     anagrams_array.each do |anagram|
-      anagram.split(" ").sort
+      anagram.split(" ")
 
     anagrams_array.detect do |anagram|
       a = anagram.split(" ")
