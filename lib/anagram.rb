c4f25ed11e4b2483919d69d326a2c9dfd044b0fe
Jung Lee <lee.jung.s95@gmail.com>
2020-05-20 02:38:10 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -14,13 +14,13 @@ class Anagram
 
     #once a word is sorted, and it matches the instance variable "word", then it's an anagram
 
-    def match(anagrams_array)
-      anagrams_array.detect do |anagram|
-        a = anagram.split(" ")
-        w = @word.split(" ")
-        return anagram if a.sort == @word.sort
-      end
+  def match(anagrams_array)
+    anagrams_array.detect do |anagram|
+      a = anagram.split(" ")
+      w = @word.split(" ")
+      return anagram if a.sort == @word.sort
     end
+  end
 
 
 
