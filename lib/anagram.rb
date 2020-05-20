fd8dcdefe654d8ae20130fce7b798ec43d627533
Jung Lee <lee.jung.s95@gmail.com>
2020-05-20 02:26:38 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,9 @@ class Anagram
   def match(anagrams_array)
     #iterate an array of anagrams
     anagrams_array.each do |anagrams|
-      anagrams.split(", ")
+      asplit = anagrams.split(", ")
+      @word = word.split(" ")
+      asplit.sort = @word.split 
     #iterate each word and sort them
     #once a word is sorted, and it matches the instance variable "word", then it's an anagram
     anagrams_array
