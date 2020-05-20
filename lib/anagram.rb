b70b98c9678ed058b9bd61f3902ed22e44679774
Jung Lee <lee.jung.s95@gmail.com>
2020-05-20 03:13:52 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,13 +7,6 @@ class Anagram
     @word = word
   end
 
-
-    #iterate each word and sort them
-    #sorted letters should equal the instance variable @word
-    #if they match then return the matching anagram
-
-    #once a word is sorted, and it matches the instance variable "word", then it's an anagram
-
   def match(anagrams_array)
     anagrams_array.each do |word|
       if word.split("").sort == @word.split("").sort
